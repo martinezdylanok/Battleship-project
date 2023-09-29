aaa69a37a79c0682f372ab8c0e11ffef59ae51fe
Lpik97 <dm97fx@gmail.com>
2023-09-29 08:45:15 +0200

    feat: Implement Ship class with hit and isSunk methods
    - Added a Ship class with a constructor to initialize the ship's length, hits count, and sunk status.
    - Implemented a hit method to record hits on the ship.
    - Implemented an isSunk method to check if the ship is sunk based on its hits count and length.

---
+++ dist/ship.js
@@ -0,0 +1,17 @@
+class Ship {
+    constructor(length) {
+        this.length = length;
+        this.hitsNum = 0;
+        this.isSunk = false;
+    }
+
+    hit() {
+        return this.hitsNum + 1;
+    }
+
+    isSunk() {
+        if (this.hitsNum === this.length) {
+            return this.isSunk = true;
+        }
+    }
+}
