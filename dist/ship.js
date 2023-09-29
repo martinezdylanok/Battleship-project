5cbcce709a50cdb589fb5c0d1f3a9fb96cf9ea52
Lpik97 <dm97fx@gmail.com>
2023-09-29 09:27:36 +0200

    refactor: ship class methods and properties
    -The isSunk property name was changed to sunkState
    -The isSunk method now has a base case returning the current state of false if the hitsNum and length are not equal.
    -If the two values match, then the sunkState changes to true

--- dist/ship.js
+++ dist/ship.js
@@ -1,19 +1,22 @@
 class Ship {
-    constructor(length) {
-        this.length = length;
-        this.hitsNum = 0;
-        this.isSunk = false;
-    }
+  constructor(length) {
+    this.length = length;
+    this.hitsNum = 0;
+    this.sunkState = false;
+  }
 
-    hit() {
-        return this.hitsNum + 1;
-    }
+  hit() {
+    return this.hitsNum + 1;
+  }
 
-    isSunk() {
-        if (this.hitsNum === this.length) {
-            return this.isSunk = true;
-        }
+  isSunk() {
+    if (this.hitsNum !== this.length) {
+        return this.sunkState;
+    } (this.hitsNum === this.length) {
+      this.sunkState = true;
+      return this.sunkState;
     }
+  }
 }
 
 module.exports = Ship;
