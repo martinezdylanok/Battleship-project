897d46f3f41371d78ec9dc648fa15fcada15f32f
Lpik97 <dm97fx@gmail.com>
2023-10-03 09:49:08 +0200

    refactor: class grid property array modified, placeShip method modified
    -The arrays order inside the grid property were modified.
    -The placeShip mathod first conditional statement was removed as the ships will be placed

--- dist/gameBoard.js
+++ dist/gameBoard.js
@@ -2,49 +2,46 @@ class Gameboard {
   constructor() {
     this.width = 10;
     this.height = 10;
-    this.grid = Array.from({ length: this.height }, () =>
-      Array.from({ length: this.width }, () => " "),
+    this.grid = Array.from({ length: this.width }, () =>
+      Array.from({ length: this.height }, () => " "),
     );
     this.missedShots = [];
     this.areAllSunk = false;
   }
 
   placeShip(x, y, ship) {
-    let isValidPlacement = false;
-    if (x >= 0 && x < this.width && y >= 0 && y < this.height) {
-      isValidPlacement = true;
-      const shipSymbol = "S";
+    let isValidPlacement = true;
+    const SHIP_SYMBOL = "S";
 
-      if (ship.position === "horizontal") {
-        if (x + ship.length - 1 >= this.width) {
-          isValidPlacement = false;
-        } else {
-          for (let i = 0; i < ship.length; i += 1) {
-            if (this.grid[x + i][y] !== " ") {
-              isValidPlacement = false;
-              break;
-            }
-          }
-          if (isValidPlacement) {
-            for (let i = 0; i < ship.length; i += 1) {
-              this.grid[x + i][y] = shipSymbol;
-            }
+    if (ship.position === "horizontal") {
+      if (x + ship.length - 1 >= this.width) {
+        isValidPlacement = false;
+      } else {
+        for (let i = 0; i < ship.length; i += 1) {
+          if (this.grid[x + i][y] !== " ") {
+            isValidPlacement = false;
+            break;
           }
         }
-      } else if (ship.position === "vertical") {
-        if (y + ship.length - 1 >= this.height) {
-          isValidPlacement = false;
-        } else {
+        if (isValidPlacement) {
           for (let i = 0; i < ship.length; i += 1) {
-            if (this.grid[x][y + i] !== " ") {
-              isValidPlacement = false;
-              break;
-            }
+            this.grid[x + i][y] = SHIP_SYMBOL;
           }
-          if (isValidPlacement) {
-            for (let i = 0; i < ship.length; i += 1) {
-              this.grid[x][y + i] = shipSymbol;
-            }
+        }
+      }
+    } else if (ship.position === "vertical") {
+      if (y + ship.length - 1 >= this.height) {
+        isValidPlacement = false;
+      } else {
+        for (let i = 0; i < ship.length; i += 1) {
+          if (this.grid[x][y + i] !== " ") {
+            isValidPlacement = false;
+            break;
+          }
+        }
+        if (isValidPlacement) {
+          for (let i = 0; i < ship.length; i += 1) {
+            this.grid[x][y + i] = SHIP_SYMBOL;
           }
         }
       }
