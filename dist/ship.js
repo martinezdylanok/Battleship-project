91118b3d1892e4f9affa20689bea4a7d7a849367
Lpik97 <dm97fx@gmail.com>
2023-10-05 07:43:42 +0200

    Refactor: assignLength function to determine ship length by type
    - Improved the readability of the assignLength function.
    - The function now uses conditional statements to assign the length based on the ship's type parameter in the constructor.
    - Added comments for clarity and documentation.

--- dist/ship.js
+++ dist/ship.js
@@ -1,34 +1,37 @@
 class Ship {
-  constructor(position, type) {
-    this.position = position;
-    this.type = type;
-    this.length = this.assignLength(type);
-    this.hitsNum = 0;
-    this.sunkState = false;
-  }
+   constructor(position, type) {
+      this.position = position;
+      this.type = type;
+      this.length = this.assignLength(type);
+      this.hitsNum = 0;
+      this.sunkState = false;
+   }
 
-  hit() {
-    this.hitsNum += 1;
-  }
+   hit() {
+      this.hitsNum += 1;
+   }
 
-  assignLength(type) {
-    if (type === "Carrier") {
-      this.length = 5;
-    } else if (type === "Battleship") {
-      this.length = 4;
-    } else if (type === "Destroyer" || type === "Submarine") {
-      this.length = 3;
-    } else if (type === "Patrol boat") {
-      this.length = 2;
-    }
-  }
+   assignLength(type) {
+      if (type === "Carrier") {
+         return 5;
+      }
+      if (type === "Battleship") {
+         return 4;
+      }
+      if (type === "Destroyer" || type === "Submarine") {
+         return 3;
+      }
+      if (type === "Patrolboat") {
+         return 2;
+      }
+   }
 
-  isSunk() {
-    if (this.hitsNum === this.length) {
-      this.sunkState = true;
-    }
-    return this.sunkState;
-  }
+   isSunk() {
+      if (this.hitsNum === this.length) {
+         this.sunkState = true;
+      }
+      return this.sunkState;
+   }
 }
 
 module.exports = Ship;
