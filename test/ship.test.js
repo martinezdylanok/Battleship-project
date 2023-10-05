008ff5743989d475ec78fbeb9a9c896cdc1f1976
Lpik97 <dm97fx@gmail.com>
2023-10-05 07:46:52 +0200

    Refactor: Update ship tests and add ship type parameter
    - Modified ship tests to include the ship type parameter in the constructor.
    - Updated the tests to check the ship's position, type, and length based on the type parameter.
    - Added tests for the hit and isSunk methods.
    - Introduced the ship type parameter in the constructor to align with the modified tests.

--- test/ship.test.js
+++ test/ship.test.js
@@ -1,26 +1,38 @@
 const Ship = require("../dist/ship.js");
 
-const SHIP = new Ship("horizontal", 5);
-
 describe("Ship creation and basic methods", () => {
-  it("Create a ship with a custom length", () => {
-    expect(SHIP.length).toBe(5);
-  });
+   it("Check ship position", () => {
+      const SHIP = new Ship("Horizontal", "Patrolboat");
+      expect(SHIP.position).toBe("Horizontal");
+   });
 
-  it("Check hit method", () => {
-    SHIP.hit();
-    expect(SHIP.hitsNum).toBe(1);
-  });
+   it("Check ship type", () => {
+      const SHIP = new Ship("Horizontal", "Battleship");
+      expect(SHIP.type).toBe("Battleship");
+   });
 
-  it("Check isSunk method", () => {
-    SHIP.isSunk();
-    expect(SHIP.sunkState).toBe(false);
-  });
+   it("Check match of ship type with its length", () => {
+      const SHIP = new Ship("Horizontal", "Patrolboat");
+      expect(SHIP.length).toBe(2);
+   });
 
-  it("Check that isSunk really checks for a sunk ship", () => {
-    const SHIP2 = new Ship("horizontal", 1);
-    SHIP2.hit();
-    SHIP2.isSunk();
-    expect(SHIP2.sunkState).toBe(true);
-  });
+   it("Check hit method", () => {
+      const SHIP = new Ship("Horizontal", "Destroyer");
+      SHIP.hit();
+      expect(SHIP.hitsNum).toBe(1);
+   });
+
+   it("Check isSunk method", () => {
+      const SHIP = new Ship("Horizontal", "Carrier");
+      SHIP.isSunk();
+      expect(SHIP.sunkState).toBe(false);
+   });
+
+   it("Check that isSunk really checks for a sunk ship", () => {
+      const SHIP2 = new Ship("Horizontal", "Patrolboat");
+      SHIP2.hit();
+      SHIP2.hit();
+      SHIP2.isSunk();
+      expect(SHIP2.sunkState).toBe(true);
+   });
 });
