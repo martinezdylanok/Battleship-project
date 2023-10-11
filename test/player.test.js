f936cd18d31b548a5d7e39920de9d9fa1be2f8df
Lpik97 <dm97fx@gmail.com>
2023-10-11 09:31:32 +0200

    refactor: gameboard was removed from the player class
    -As gameboard property was removed from the player class all test cases use a new instance of the Gameboard object to test the conditions.

--- test/player.test.js
+++ test/player.test.js
@@ -10,17 +10,19 @@ describe("Player class tests", () => {
 
    it("Should place all the ships on the game board", () => {
       const PLAYER = new Player("George");
-      PLAYER.placeShips();
-      expect(PLAYER.gameBoard.ships.length).toBe(5);
+      const GAMEBOARD = new Gameboard();
+      PLAYER.placeShips(GAMEBOARD);
+      expect(GAMEBOARD.ships.length).toBe(5);
    });
 
    it("Should simulate a random attack on an empty gameboard", () => {
       const PLAYER = new Player("Michael");
-      PLAYER.shootAtCoordinates();
+      const GAMEBOARD = new Gameboard();
+      PLAYER.shootAtCoordinates(GAMEBOARD);
       let foundOne = false;
-      for (let x = 0; x < PLAYER.gameBoard.width; x += 1) {
-         for (let y = 0; y < PLAYER.gameBoard.height; y += 1) {
-            if (PLAYER.gameBoard.grid[x][y] === "O") {
+      for (let x = 0; x < GAMEBOARD.width; x += 1) {
+         for (let y = 0; y < GAMEBOARD.height; y += 1) {
+            if (GAMEBOARD.grid[x][y] === "O") {
                foundOne = true;
                break;
             }
@@ -32,10 +34,11 @@ describe("Player class tests", () => {
    it("Should attack an adjacent cell once it hits a ship cell", () => {
       const PLAYER = new Player("Arnold");
       const SHIP = new Ship("Horizontal", "Patrolboat");
-      PLAYER.gameBoard.placeShip(1, 0, SHIP);
-      PLAYER.gameBoard.grid[1][0] = "X";
+      const GAMEBOARD = new Gameboard();
+      GAMEBOARD.placeShip(1, 0, SHIP);
+      GAMEBOARD.grid[1][0] = "X";
 
-      PLAYER.shootAtCoordinates();
+      PLAYER.shootAtCoordinates(GAMEBOARD);
 
       const ADJACENT_CELLS = [
          [0, 0],
@@ -48,7 +51,7 @@ describe("Player class tests", () => {
          [2, 2],
       ];
 
-      const ANY_ADJACENT_CELL_CONTAINS_X = ADJACENT_CELLS.some(([x, y]) => PLAYER.gameBoard.grid[x][y] === "X");
+      const ANY_ADJACENT_CELL_CONTAINS_X = ADJACENT_CELLS.some(([x, y]) => GAMEBOARD.grid[x][y] === "X");
       expect(ANY_ADJACENT_CELL_CONTAINS_X).toBe(true);
    });
 });
