09ae342541239f76a984efaa5fb5a1a94755a550
Lpik97 <dm97fx@gmail.com>
2023-10-23 11:00:11 +0200

    refactor: Updated Player class for improved functionality and readability
    - Removed 'placeShips' method and replaced it with 'autoPlaceShips' for clarity and consistency in ship placement.
    - Added 'randomAttack' method for AI players to make random attacks.
    - Added 'strategicAttack' method for AI players to make strategic attacks based on adjacent hits.
    - Reorganized the code for better readability and maintainability.
    - Renamed 'placeShips' to 'autoPlaceShips' for clarity.
    - Improved comments and variable naming for better code understanding.

--- dist/player.js
+++ dist/player.js
@@ -7,53 +7,73 @@ class Player {
       this.isTurn = false;
    }
 
-   placeShips(gameboard) {
-      if (this.isUser === false) {
-         const SHIP_TYPES = ["Patrolboat", "Submarine", "Destroyer", "Battleship", "Carrier"];
-         const SHIP_POSITIONS = ["Horizontal", "Vertical"];
+   autoPlaceShips(gameboard) {
+      const SHIP_TYPES = ["Patrolboat", "Submarine", "Destroyer", "Battleship", "Carrier"];
+      const SHIP_POSITIONS = ["Horizontal", "Vertical"];
 
-         while (gameboard.ships.length < 5) {
-            const X = Math.floor(Math.random() * gameboard.width);
-            const Y = Math.floor(Math.random() * gameboard.height);
-            const RANDOM_SHIP_TYPE = SHIP_TYPES[Math.floor(Math.random() * SHIP_TYPES.length)];
-            const RANDOM_SHIP_POSITION = SHIP_POSITIONS[Math.floor(Math.random() * SHIP_POSITIONS.length)];
-            const SHIP = new Ship(RANDOM_SHIP_POSITION, RANDOM_SHIP_TYPE);
-            gameboard.placeShip(X, Y, SHIP);
-         }
+      while (gameboard.ships.length < 5) {
+         const X = Math.floor(Math.random() * gameboard.width);
+         const Y = Math.floor(Math.random() * gameboard.height);
+         const RANDOM_SHIP_TYPE = SHIP_TYPES[Math.floor(Math.random() * SHIP_TYPES.length)];
+         const RANDOM_SHIP_POSITION = SHIP_POSITIONS[Math.floor(Math.random() * SHIP_POSITIONS.length)];
+         const SHIP = new Ship(RANDOM_SHIP_POSITION, RANDOM_SHIP_TYPE);
+         gameboard.placeShip(X, Y, SHIP);
       }
    }
 
-   shootAtCoordinates(gameboard) {
-      if (this.isUser === false) {
-         for (let x = 0; x < gameboard.width; x += 1) {
-            for (let y = 0; y < gameboard.height; y += 1) {
-               if (gameboard.grid[x][y] === "X") {
-                  const OFFSETS = [
-                     [-1, 0],
-                     [1, 0],
-                     [0, -1],
-                     [0, 1],
-                     [-1, -1],
-                     [-1, 1],
-                     [1, -1],
-                     [1, 1],
-                  ];
+   randomAttack(gameboard, opponent, player) {
+      if (this.isUser || !this.isTurn) return [null, null];
 
-                  for (let i = 0; i < OFFSETS.length; i += 1) {
-                     const DX = OFFSETS[i][0];
-                     const DY = OFFSETS[i][1];
-                     const X = x + DX;
-                     const Y = y + DY;
-                     gameboard.receiveAttack(X, Y);
-                  }
-               } else {
-                  const X = Math.floor(Math.random() * gameboard.width);
-                  const Y = Math.floor(Math.random() * gameboard.height);
+      const X = Math.floor(Math.random() * gameboard.width);
+      const Y = Math.floor(Math.random() * gameboard.height);
+
+      gameboard.receiveAttack(X, Y);
+      const AI = player;
+      AI.isTurn = false;
+      const USER = opponent;
+      USER.isTurn = true;
+
+      return [X, Y];
+   }
+
+   async strategicAttack(gameboard, opponent, player) {
+      if (this.isUser || !this.isTurn) return [null, null];
+
+      let foundHit = false;
+      let attackCoordinates = [null, null];
+
+      for (let x = 0; x < gameboard.width; x += 1) {
+         for (let y = 0; y < gameboard.height; y += 1) {
+            if (gameboard.grid[x][y] === "X" && !foundHit) {
+               const ADJACENT_COORDS = [
+                  [x - 1, y],
+                  [x + 1, y],
+                  [x, y - 1],
+                  [x, y + 1],
+               ];
+
+               const randomIndex = Math.floor(Math.random() * ADJACENT_COORDS.length);
+               const [X, Y] = ADJACENT_COORDS[randomIndex];
+
+               if (X >= 0 && X < gameboard.width && Y >= 0 && Y < gameboard.height && gameboard.grid[X][Y] !== "O" && gameboard.grid[X][Y] !== "X") {
                   gameboard.receiveAttack(X, Y);
+                  foundHit = true;
+                  const AI = player;
+                  AI.isTurn = false;
+                  const USER = opponent;
+                  USER.isTurn = true;
+                  attackCoordinates = [X, Y];
                }
             }
+            if (foundHit) break;
          }
+         if (foundHit) break;
       }
+
+      if (!foundHit) {
+         attackCoordinates = await this.randomAttack(gameboard, opponent, player);
+      }
+      return attackCoordinates;
    }
 }
 
