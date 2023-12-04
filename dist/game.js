406e52dac243f486ccc46a751304cf315426ccf1
Lpik97 <dm97fx@gmail.com>
2023-12-04 19:07:46 +0100

    feat[game-refactor]: refactor GAME function using Helpers and new UI structure
    - Reorganized the GAME function to leverage the Helpers class for creating and displaying game-related elements
    - Utilized the createGrid and displayGrid methods from Helpers to initialize and display user and AI gameboards
    - Integrated drag-and-drop functionality for placing user ships on the gameboard using dragUserShips method
    - Implemented ship rotation for user ships with rotateUserShips method
    - Utilized the new structure of the Helpers class to simplify and improve the overall readability of the GAME function

--- dist/game.js
+++ dist/game.js
@@ -1,43 +1,25 @@
 import Gameboard from "./gameBoard.js";
+import Helpers from "./helpers.js";
 import Player from "./player.js";
-import Dom from "./dom.js";
 
 const GAME = () => {
-   const DOM = new Dom();
-
+   const HELPERS = new Helpers();
    const USER_GAMEBOARD = new Gameboard();
    const AI_GAMEBOARD = new Gameboard();
-
-   const USER = new Player("User");
+   const USER = new Player();
+   const AI = new Player();
    USER.isUser = true;
    USER.isTurn = true;
-   const AI = new Player("AI");
 
-   USER.autoPlaceShips(USER_GAMEBOARD);
-   AI.autoPlaceShips(AI_GAMEBOARD);
+   const USER_GRID = HELPERS.createGrid(USER_GAMEBOARD, USER);
+   const AI_GRID = HELPERS.createGrid(AI_GAMEBOARD, AI);
 
-   DOM.displayGrid(USER_GAMEBOARD, USER);
-   DOM.displayGrid(AI_GAMEBOARD, AI);
+   HELPERS.displayGrid(USER, USER_GRID);
+   HELPERS.displayGrid(AI, AI_GRID);
 
-   const gameLoop = setInterval(async () => {
-      if (USER.isTurn) {
-         const PREVIOUS_SHOTS = AI_GAMEBOARD.missedShots;
-         const RESULT = await DOM.userAttack(AI_GAMEBOARD, AI, USER);
-         const X = RESULT[0];
-         const Y = RESULT[1];
-         DOM.markAttackedCell(AI_GAMEBOARD, AI, PREVIOUS_SHOTS, X, Y);
-      } else if (AI.isTurn) {
-         const PREVIOUS_SHOTS = USER_GAMEBOARD.missedShots;
-         const RESULT = await AI.strategicAttack(USER_GAMEBOARD, USER, AI);
-         const X = RESULT[0];
-         const Y = RESULT[1];
-         DOM.markAttackedCell(USER_GAMEBOARD, USER, PREVIOUS_SHOTS, X, Y);
-      }
+   const INITIAL_SHIPS_ELEMENTS = HELPERS.SHIPS_ELEMENTS;
+   const INITIAL_SHIPS_OBJECTS = HELPERS.SHIPS_OBJECTS;
 
-      if (USER_GAMEBOARD.areAllSunk || AI_GAMEBOARD.areAllSunk) {
-         clearInterval(gameLoop);
-      }
-   }, 1000);
+   HELPERS.dragUserShips(USER_GAMEBOARD, INITIAL_SHIPS_ELEMENTS, INITIAL_SHIPS_OBJECTS);
+   HELPERS.rotateUserShips(INITIAL_SHIPS_ELEMENTS, INITIAL_SHIPS_OBJECTS);
 };
-
-export default GAME;
