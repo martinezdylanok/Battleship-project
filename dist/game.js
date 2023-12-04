7f94241922b17ac050430b8aa58f95646d96ab94
Lpik97 <dm97fx@gmail.com>
2023-12-04 20:38:13 +0100

    refactor(js): remove turn assignment in the initial game setup
    - Removed the assignment of turns in the early stage of the game, as it is not necessary.

--- dist/game.js
+++ dist/game.js
@@ -8,8 +8,6 @@ const GAME = () => {
    const AI_GAMEBOARD = new Gameboard();
    const USER = new Player();
    const AI = new Player();
-   USER.isUser = true;
-   USER.isTurn = true;
 
    const USER_GRID = HELPERS.createGrid(USER_GAMEBOARD, USER);
    const AI_GRID = HELPERS.createGrid(AI_GAMEBOARD, AI);
