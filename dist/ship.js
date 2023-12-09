3af7d4592eb291f990757bb4912d28484bd5df21
Lpik97 <dm97fx@gmail.com>
2023-12-09 14:31:34 +0100

    refactor(ship): Use shipLengthAssigner for length assignment
    - Removed the assignLength method from the Ship class and incorporated the ship length assignment logic from the shipLengthAssigner module.

--- dist/ship.js
+++ dist/ship.js
@@ -1,8 +1,10 @@
+import shipLengthAssigner from "./shipLengthAssigner.js";
+
 class Ship {
    constructor(position, type) {
       this.position = position;
       this.type = type;
-      this.length = Ship.assignLength(type);
+      this.length = shipLengthAssigner.assignShipLength(type);
       this.hitsNum = 0;
       this.sunkState = this.isSunk();
    }
@@ -10,22 +12,6 @@ class Ship {
    hit() {
       this.hitsNum += 1;
       this.sunkState = this.isSunk();
-   }
-
-   static assignLength(type) {
-      switch (type) {
-         case "Carrier":
-            return 5;
-         case "Battleship":
-            return 4;
-         case "Destroyer":
-         case "Submarine":
-            return 3;
-         case "Patrolboat":
-            return 2;
-         default:
-            throw new Error(`Unknown ship type: ${type}`);
-      }
    }
 
    isSunk() {
