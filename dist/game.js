60ff3f1b081235694e1453f8c0f44a340da079b2
Lpik97 <dm97fx@gmail.com>
2023-12-11 14:15:28 +0100

    refactor(game): restructure 'GAME' module initialization
    - Refactor the initialization of game objects and their initial state.
    - Use 'storeGameData' method from 'Helpers' for managing player and gameboard data.
    - Enhance code organization and readability.
    - Remove unnecessary intermediate variables.

--- dist/game.js
+++ dist/game.js
@@ -3,25 +3,16 @@ import Helpers from "./helpers.js";
 import Player from "./player.js";
 
 const GAME = () => {
-   const HELPERS = new Helpers();
    const USER_GAMEBOARD = new Gameboard();
    const AI_GAMEBOARD = new Gameboard();
    const USER = new Player();
    const AI = new Player();
+   const HELPERS = new Helpers();
+
    USER.isTurn = true;
    USER.isUser = true;
 
-   const USER_GRID = HELPERS.createGrid(USER_GAMEBOARD, USER);
-   const AI_GRID = HELPERS.createGrid(AI_GAMEBOARD, AI);
-
-   HELPERS.displayGrid(USER_GRID, USER);
-   HELPERS.displayGrid(AI_GRID, AI);
-
-   const INITIAL_SHIPS_ELEMENTS = HELPERS.SHIPS_ELEMENTS;
-   const INITIAL_SHIPS_OBJECTS = HELPERS.SHIPS_OBJECTS;
-
-   HELPERS.dragUserShips(USER_GAMEBOARD, INITIAL_SHIPS_ELEMENTS, INITIAL_SHIPS_OBJECTS);
-   HELPERS.rotateUserShips(INITIAL_SHIPS_ELEMENTS, INITIAL_SHIPS_OBJECTS);
+   HELPERS.storeGameData(USER, AI, USER_GAMEBOARD, AI_GAMEBOARD);
 };
 
 export default GAME();
